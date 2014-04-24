81837c5efaa574cc735e9473298129dfc734f399
Alberto Riva <alb.bbfge@gmail.com>
2014-04-23 23:19:48 -0400

    many many changes

--- hits.py
+++ hits.py
@@ -1,4 +1,60 @@
+import cStringIO
+
 ### Class representing Mapper hits
 
 class Hit():
-    
+    GeneID = ""
+    Gene = ""
+    Org = ""
+    Accession = ""
+    Model = ""
+    Factor = ""
+    Strand = ""
+    Chrom = ""
+    Start = 0
+    End = 0
+    Region = ""
+    Score = 0
+    Eval = 0
+    Conserved = False
+    Alignment_model = ""
+    Alignment_match = ""
+    Alignment_sequence = ""
+
+    def initHit(self, data):
+        # Fill slots of this hit using values from the list `data'
+        # (obtained by parsing a MAPPER csv)
+        self.GeneID = data[0]
+        self.Gene = data[1]
+        self.Org = data[2]
+        self.Accession = data[3]
+        self.Model = data[4]
+        self.Factor = data[5]
+        self.Strand = data[6]
+        self.Chrom = data[7]
+        self.Start = int(data[8])
+        self.End = int(data[9])
+        self.Region = data[10]
+        self.Score = float(data[11])
+        self.Eval = float(data[12])
+        self.Conserved = data[13]
+        self.Alignment = data[14]
+
+class Hitset():
+    hits = []
+    nhits = 0
+
+    def initHitsetFromCsv(self, n, data):
+        self.nhits = n
+        reader = cStringIO.StringIO(data)
+        reader.readline()       # skip header
+        for i in range(n):
+            h = Hit()
+            d1 = reader.readline().rstrip("\n").split("\t") #  read main line
+            d2 = reader.readline().rstrip("\n").split("\t") # read line containing alignment match
+            d3 = reader.readline().rstrip("\n").split("\t") # read line containing alignment sequence
+            reader.readline()                             # skip empty line
+            d1[14] = (d1[14], d2[14], d3[14])
+            h.initHit(d1)
+            self.hits.append(h)
+        return self.hits
--- pymapper.py
+++ pymapper.py
@@ -1,19 +1,12 @@
 import string
 import httplib
+import hits
+
+### Globals
 
 mapperHostname = "genome.ufl.edu"
 mapperUrl = "/mapper/db-rpc"
 toolName = "pymapper-1.0"
-pymapperDebug = False
-
-class MapperParams():
-    score  = False
-    perc   = False
-    evalue = False
-    pbases = False
-    pstart = False
-    org    = False
-    sort   = False
 
 ### Utils
 
@@ -23,43 +16,106 @@ def splitFirstLine(data):
     eol = string.find(data, "\n")
     return (data[0:eol], data[eol+1:])
 
-### Base methods
-
 def sendMapperRequest(request):
+    # Sends `request' to the MAPPER server.
+    # Returns the body of the response as a string.
     conn = httplib.HTTPConnection(mapperHostname)
     conn.request("GET", request)
     response = conn.getresponse()
     conn.close()
     return response.read()
     
-def parseMapperResponse(data):
-    (n, body) = splitFirstLine(data)
-    n = int(n)
-    if n < 0:
-        (errmsg, rest) = splitFirstLine(body)
-        print("Error: {}".format(errmsg))
-        return False
-    else:
-        if pymapperDebug:
-            print("{} records returned.".format(n))
-        return (n, body)
-
-def saveMapperRequest(data, filename):
+def saveMapperResponse(data, filename):
     # Save `data' returned by a mapper rpc-call to `filename'
-    # stripping the 
-    (n, body) = parseMapperResponse(data)
     with open(filename, "w") as f:
-        f.write(body)
+        f.write(data)
 
-def getMapperList(prefix="*"):
-    request = mapperUrl + "?list=" + prefix
-    if pymapperDebug:
-        print("Submitting request: " + request)
-    return sendMapperRequest(request)
+### Client class
 
-def sendMapperHits(gene, models):
-    request = mapperUrl + "?gene=" + gene + "&models=" + models
-    if pymapperDebug:
-        print("Submitting request: " + request)
-    return sendMapperRequest(request)
+class MapperDbClient():
+    # Request optional parameters
+    score  = False
+    perc   = False
+    evalue = False
+    pbases = False
+    pstart = False
+    org    = False
+    sort   = False
+    # Internal
+    Debug  = False
+
+### Base methods
+
+    def makeParamsUrl(self):
+        url = "&tool=" + toolName
+        if self.score:
+            url = url + "&score=" + str(self.score)
+        if self.perc:
+            url = url + "&perc=" + str(self.perc)
+        if self.evalue:
+            url = url + "&eval=" + str(self.evalue)
+        if self.pbases:
+            url = url + "&pbases=" + str(self.pbases)
+        if self.pstart:
+            url = url + "&pstart=" + str(self.pstart)
+        if self.org:
+            url = url + "&org=" + self.org
+        if self.sort:
+            url = url + "&sort=" + self.sort
+        return url
+
+    def parseMapperResponse(self, data):
+        # Parse a MAPPER response `data'. Extract the first line, convert
+        # it to int, and signal an error if it is negative. Otherwise
+        # returns the tuple (n, body) where n is the value of the first line.
+        (n, body) = splitFirstLine(data)
+        n = int(n)
+        if n < 0:
+            (errmsg, rest) = splitFirstLine(body)
+            print("Error: {}".format(errmsg))
+            return False
+        else:
+            if self.Debug:
+                print("{} records returned.".format(n))
+            return (n, body)
+
+    def getMapperList(self, prefix="*"):
+        request = mapperUrl + "?list=" + prefix
+        if self.Debug:
+            print("Submitting request: " + request)
+        return sendMapperRequest(request)
+
+    def getMapperHits(self, gene, models):
+        request = mapperUrl + "?gene=" + gene + "&models=" + models + self.makeParamsUrl()
+        if self.Debug:
+            print("Submitting request: " + request)
+        return sendMapperRequest(request)
+
+    # Top-level methods
+
+    def modelsToFile(self, filename, prefix="*", debug=False):
+        if debug:
+            self.Debug = debug
+        (n, body) = self.parseMapperResponse(self.getMapperList(prefix))
+        if self.Debug:
+            print("Saving models to file {}".format(filename))
+        saveMapperResponse(body, filename)
+        return n
+
+    def hitsToFile(self, filename, gene, models, debug=False):
+        if debug:
+            self.Debug = debug
+        (n, body) = self.parseMapperResponse(self.getMapperHits(gene, models))
+        if self.Debug:
+            print("Saving hits to file {}".format(filename))
+        saveMapperResponse(body, filename)
+        return n
+
+    def hitsToHitset(self, gene, models, debug=False):
+        if debug:
+            self.Debug = debug
+        (n, body) = self.parseMapperResponse(self.getMapperHits(gene, models))
+        hitset = hits.Hitset()
+        hitset.initHitsetFromCsv(n, body)
+        return hitset
 
